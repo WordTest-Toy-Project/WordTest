c40e4b3e06af16e1abfe9b4798e643e8b6e43df1
morningB <yjw001205@gmail.com>
2023-12-12 15:26:16 +0900

    reset

--- src/App.js
+++ src/App.js
@@ -2,13 +2,7 @@ import './App.css';
 
 function App() {
   return (
-     <dib>
-      <h1>Word Test</h1>
-      <div>
-        <input className='signin' type='text' placeholder='id'></input>
-        <input className='signin' type='text' placeholder='password'></input>
-      </div>
-     </dib>
+     <></>
   );
 }
 

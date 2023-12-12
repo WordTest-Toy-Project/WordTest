b4083bd85123d96c633416b77c3c82d5518ea89d
morningB <yjw001205@gmail.com>
2023-12-12 16:25:24 +0900

    a

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import './App.css';
 
+
 function App() {
   return (
-    <>abcefg</>
+    <ss>asdas</ss>
   );
 }
 
--- src/pages/main/Main.js
+++ src/pages/main/Main.js
@@ -1,4 +1,10 @@
+import styled from "styled-components";
+
 export default function Main() {
+  const MainInput = styled.input`
+    color:white;
+    border-color: #892CDC;
+  `;
   return (
     <>hihi</>
   );

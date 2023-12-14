ab83b6d82317695b328072f3300747f9d781622c
Ji-Sung05 <dlwltjd6847@gmail.com>
2023-12-14 14:48:36 +0900

    Add: test-result 페이지 생성

--- src/pages/test_result/Test_result.js
+++ src/pages/test_result/Test_result.js
@@ -1,5 +1,31 @@
+import Header from '../../components/header/Header';
+import Title from '../../components/title/Title';
+
+import {
+  Desc,
+  Result,
+} from './style';
+
 export default function Test_result() {
+  const sampleJson = [
+    {
+      id: 0,
+      word: "Apple",
+      mean: "사과"
+    },
+    {
+      id: 1,
+      word: "Banana",
+      mean: "바나나"
+    }
+  ]
+  sampleJson;
     return (
-      <>Test_result</>
+      <>
+        <Header />
+        <Title title={"Word Test"}/>
+        <Result><span style={{color: "red"}}>3</span> 문제 정답!</Result>
+        <Desc><span style={{color: "yellow"}}>오답 단어</span></Desc>
+      </>
     );
   }
---
+++ src/pages/test_result/style.js
@@ -0,0 +1,21 @@
+import styled from 'styled-components';
+
+export const Desc = styled.div`
+  width: 850px;
+  height: 120px;
+  margin: 0 auto;
+  border: 10px solid #892CDC;
+  border-radius: 10px;
+  margin-top: 70px;
+  font-size: 80px;
+  text-align: center;
+  color: white;
+`;
+
+export const Result = styled.div`
+  width: 850px;
+  margin: 0 auto;
+  font-size: 50px;
+  color: #B6B3B3;
+  padding: 0 20px;
+`;

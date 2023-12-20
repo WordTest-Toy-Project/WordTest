df13308797eba783d5ed20f10e807fb9ef2bc904
Ji-Sung05 <dlwltjd6847@gmail.com>
2023-12-20 15:34:48 +0900

    Modify: sample.json 객체 수정

--- client/src/pages/login/Signin.js
+++ client/src/pages/login/Signin.js
@@ -3,46 +3,24 @@ import { Desc, InputContainer, SignBtn } from "./style";
 //library
 import { useNavigate } from "react-router-dom";
 import { useState } from "react";
-import axios from "axios";
 //components
 import LoginInput from "../../components/input/LoginInput";
 import Title from "../../components/title/Title";
 import Fotter from "../../components/footer/Footer";
+//sampleData
+import sampleJson from '../../sample.json';
 
 export default function Signin() {
   const navigate = useNavigate();
-
+  console.log("샘플 데이터 확인: ", sampleJson);
   const [user, setUser] = useState({
     username: "",
     password: "",
   });
 
-  const LoginHandler = async () => {
-    try {
-      const response = await axios.post(
-        `${process.env.REACT_APP_LOCAL_URL}/signin`,
-        {
-          username: user.username,
-          password: user.password,
-        },
-      );
-      if (response.status === 200) {
-        console.log("로그인 성공!");
-        localStorage.setItem("user", JSON.stringify(user));
-        alert("로그인 성공!");
-        navigate("/main");
-      } else {
-        console.error("로그인 실패");
-      }
-    } catch (error) {
-      // 에러가 발생했을 때 에러 메시지 출력
-      if (axios.isAxiosError(error)) {
-        console.error("Axios 에러:", error.response?.data || error.message);
-      } else {
-        console.error("네트워크 오류:", error.message);
-      }
-    }
-  };
+  const LoginHandler = () => {
+    
+  }
 
   return (
     <>

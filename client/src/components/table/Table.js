bbd694e3d531d80622c244c17927fd595d538d46
Ji-Sung05 <dlwltjd6847@gmail.com>
2023-12-21 15:51:42 +0900

    Add: Table 삭제 기능 구현

--- client/src/components/table/Table.js
+++ client/src/components/table/Table.js
@@ -1,26 +1,27 @@
 import React, { useEffect, useState } from "react";
-import {
-  TableContainer,
-  Row0,
-  Row1,
-  Row2,
-  Row3,
-  Img,
-} from "./style";
+import { TableContainer, Row0, Row1, Row2, Row3, Img } from "./style";
 
 export default function Table({ storedUser }) {
-  
   const [deletedUserId, setDeletedUserId] = useState(null);
+  const [filteredUser, setFilteredUser] = useState(storedUser);
+
   useEffect(() => {
     if (deletedUserId !== null) {
+      // 삭제된 사용자를 필터링하여 상태 업데이트
       const updatedUser = storedUser.filter((_, index) => index !== deletedUserId);
+      setFilteredUser(updatedUser);
       localStorage.setItem("delete", JSON.stringify(updatedUser));
     }
   }, [deletedUserId, storedUser]);
 
+  const handleDelete = (index) => {
+    // 삭제된 사용자의 인덱스를 설정하여 useEffect를 트리거
+    setDeletedUserId(index);
+  };
+
   return (
     <>
-      {storedUser.map((test, index) => (
+      {filteredUser.map((test, index) => (
         <TableContainer key={index}>
           <Row0>
             <Image test={test} userId={index} />
@@ -32,7 +33,7 @@ export default function Table({ storedUser }) {
               src="./image/xbtn.png"
               alt="삭제"
               width="35px"
-              onClick={() => setDeletedUserId(index)}
+              onClick={() => handleDelete(index)}
             />
           </Row3>
         </TableContainer>
@@ -43,6 +44,5 @@ export default function Table({ storedUser }) {
 
 // Image 컴포넌트
 function Image(props) {
-
   return <Img $isScrap={props.test.is_scrap} />;
 }

6dd6978941bd9de1b66cf226da296169fbb702cc
Ji-Sung05 <dlwltjd6847@gmail.com>
2023-12-21 16:43:06 +0900

    Add: Favorite 스크랩한 단어만 렌더링 기능 추가

--- client/src/pages/favorite/Favorite.js
+++ client/src/pages/favorite/Favorite.js
@@ -5,14 +5,14 @@ import { Title } from "./style";
 import sampleJson from '../../sample.json';
 export default function Favorite() {
   //Favorite 페이지에서 단어 정보를 WordTable->Table로 전달
-    console.log(sampleJson[0].words);
+    const favoriteWords = sampleJson[0].words.filter(word => word.is_scrap);
   return (
     <>
       <Header $back={true} $title={true} $addWord={false} $gear={false} />
       <Title>
         즐겨찾기 <span style={{ color: "yellow" }}>단어</span>
       </Title>
-      <WordTable storedUser={sampleJson}/>
+      <WordTable storedUser={favoriteWords}/>
       {/* sampleJson[0] 과 sampleJson만 했을때의 차이 x */}
     </>
   );

f3bae16a07dd363845170d830c8a31578865810b
morningB <yjw001205@gmail.com>
2023-12-21 16:28:59 +0900

    Fix : favorite.js

--- client/src/pages/favorite/Favorite.js
+++ client/src/pages/favorite/Favorite.js
@@ -2,33 +2,18 @@ import Header from "../../components/header/Header";
 import WordTable from "../../containers/wordTable/WordTable";
 
 import { Title } from "./style";
-
+import sampleJson from '../../sample.json';
 export default function Favorite() {
   //Favorite 페이지에서 단어 정보를 WordTable->Table로 전달
-  const sampleJson = [
-    {
-      id: 0,
-      word: "Apple",
-      meaning: "사과",
-    },
-    {
-      id: 1,
-      word: "Banana",
-      meaning: "바나나",
-    },
-    {
-      id: 2,
-      word: "Chicken",
-      meaning: "치킨",
-    },
-  ];
+    console.log(sampleJson[0].words);
   return (
     <>
       <Header $back={true} $title={true} $addWord={false} $gear={false} />
       <Title>
         즐겨찾기 <span style={{ color: "yellow" }}>단어</span>
       </Title>
-      <WordTable studyWord={sampleJson} />
+      <WordTable storedUser={sampleJson}/>
+      {/* sampleJson[0] 과 sampleJson만 했을때의 차이 x */}
     </>
   );
 }
